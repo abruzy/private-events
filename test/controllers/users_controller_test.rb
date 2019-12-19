76f989730087ce1bf98981308a4fed712a41979a
Abubakar Diallo <dialloabubakar5861@gmail.com>
2019-12-19 23:42:12 +0100

    feat: user, event model test and user controller test added

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -3,18 +3,19 @@
 require 'test_helper'
 
 class UsersControllerTest < ActionDispatch::IntegrationTest
+
+  test 'should get the home page' do
+    get '/'
+    assert_response :success
+  end
+
   test 'should get new' do
-    get users_new_url
+    get '/sign-up'
     assert_response :success
   end
 
-  test 'should get create' do
-    get users_create_url
-    assert_response :success
-  end
-
-  test 'should get show' do
-    get users_show_url
+  test 'should get the login page' do
+    get '/login'
     assert_response :success
   end
 end
--- test/models/event_test.rb
+++ test/models/event_test.rb
@@ -3,7 +3,35 @@
 require 'test_helper'
 
 class EventTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @user = User.new(name: 'john', email: 'johndoe1@gmail.com', password: '1234562', password_confirmation: '1234562')
+    @user.save
+
+    @event = @user.events.new(title: 'Here we come', details: 'Tonight is gonna be mad oh!', date: '2020-10-12', location: 'Ilorin')
+  end
+
+  test 'should be valid' do
+    assert @event.valid?
+  end
+
+  test 'details must be present' do
+    @event.details = ''
+    assert_not @event.valid?
+  end
+
+  test 'details should be present' do
+    @event.details = 'Here we come again'
+    assert @event.valid?
+  end
+
+  test 'event should belong to user' do
+    @event.save
+    user_name = @user.name
+    assert_equal user_name ,@event.user.name
+  end
+
+  test 'date must be present' do
+    @event.details = ''
+    assert_not @event.valid?
+  end
 end
